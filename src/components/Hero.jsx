487f9bde0eb173c737497ed1a2810963c6bc1e71
Sheri Lu <sherisherilu@gmail.com>
2023-09-18 16:55:52 -0700

    Add a description paragraph on the index page.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,9 +17,13 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br />
+        Summarize Articles with <br className='max-md:hidden' />
         <span className='orange_gradient'> OpenAI GPT-4</span>
       </h1>
+
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   )
 }
