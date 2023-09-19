84a48d440e2118943d904e2810d32770d7c21390
Sheri Lu <sherisherilu@gmail.com>
2023-09-19 15:58:22 -0700

    Add function to handle data from API.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: '',
     summary: '',
-  })
+  });
 
-  //fetch API
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+
   const handleSubmit = async (e) => {
-    alert('submitted!!!✌️')
+    e.preventDefault();
+    
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      setArticle(newArticle);
+
+      console.log('newArticle in Demo: ', newArticle);
+    }
   }
 
   return (
