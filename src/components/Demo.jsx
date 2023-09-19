390eac733ad7ce3c1efb6c0dfb6b08fc8ee8a8b9
Sheri Lu <sherisherilu@gmail.com>
2023-09-18 19:13:42 -0700

    Add handleSubmit function.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,13 +3,23 @@ import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  //fetch API
+  const handleSubmit = async (e) => {
+    alert('submitted!!!✌️')
+  }
+
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
       <div className='flex flex-col w-full gap-2'>
         <form 
           className='relative flex justify-center items-center'
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img 
             src={linkIcon}
@@ -20,8 +30,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder='Enter a URL'
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className='url_input peer'
           />
@@ -33,7 +43,12 @@ const Demo = () => {
             ➡️
           </button>
         </form>
+
+        {/* Browse URL History */}
+
       </div>
+
+      {/* Display Results */}
     </section>
   )
 }
