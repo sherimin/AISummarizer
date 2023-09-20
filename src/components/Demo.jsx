a35c45aa46f12d23a47909d9b1578f20d5958ed0
Sheri Lu <sherisherilu@gmail.com>
2023-09-19 17:04:58 -0700

    Save articles and summaries to localStorage.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,20 +9,33 @@ const Demo = () => {
     summary: '',
   });
 
+  const [allArticles, setAllArticles] = useState([]);
+
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles')
+    )
+
+    if(articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, [])
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     const { data } = await getSummary({ articleUrl: article.url });
 
     if(data?.summary) {
       const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
 
-      console.log('newArticle in Demo: ', newArticle);
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
     }
   }
 
